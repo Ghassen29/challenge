efd200e374c9dc0a00f98c221571b512e8b3e950
ghassen mejri <[ghasen29@gmail.com]>
2022-10-02 19:50:31 +0100

    challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Header from './component/Header';
+import Projects from './component/Projects';
+import About from './component/About';
+import Contact from './component/Contact';
+import Footer from './component/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+<Header></Header>
+<About></About>
+<Projects></Projects>
+<Contact></Contact>
+<Footer></Footer>
     </div>
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css';
+
+const footer = () => {
+  return (
+    <div><p>Copyritghts &copy; GOMYCODE 2020</p></div>
+  )
+}
+
+export default footer
